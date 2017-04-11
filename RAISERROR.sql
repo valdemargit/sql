c3370f2504f38ef0ec06fb2e1b79febbb9d8f8d4
valdemargit <valdemar.bykov@icloud.com>
2017-04-11 10:22:35 +0300

    Add files via upload

---
+++ RAISERROR.sql
@@ -0,0 +1,6 @@
+RAISERROR ('test severity 14', 14, 1) with log; 
+RAISERROR ('test severity 18', 18, 1) with log; 
+RAISERROR ('test severity 19', 19, 1) with log; 
+RAISERROR ('test severity 21', 21, 1) with log; 
+RAISERROR ('test severity 26', 26, 1) with log; 
+RAISERROR ('test severity 27', 27, 1) with log; 
---
+++ query.ps1
@@ -0,0 +1,15 @@
+#sql2008
+Invoke-Sqlcmd -InputFile ".\RAISERROR.sql" -ServerInstance "192.168.0.250,60008" -username "sa" -password "1qaz@WSX3edc"
+Invoke-Sqlcmd -InputFile ".\RAISERROR.sql" -ServerInstance "192.168.0.250,60009" -username "sa" -password "1qaz@WSX3edc"
+
+#sql2012
+Invoke-Sqlcmd -InputFile ".\RAISERROR.sql" -ServerInstance "192.168.0.250,60012" -username "sa" -password "1qaz@WSX3edc"
+Invoke-Sqlcmd -InputFile ".\RAISERROR.sql" -ServerInstance "192.168.0.250,60013" -username "sa" -password "1qaz@WSX3edc"
+
+#sql2014
+Invoke-Sqlcmd -InputFile ".\RAISERROR.sql" -ServerInstance "192.168.0.250,60014" -username "sa" -password "1qaz@WSX3edc"
+Invoke-Sqlcmd -InputFile ".\RAISERROR.sql" -ServerInstance "192.168.0.250,60015" -username "sa" -password "1qaz@WSX3edc"
+
+#sql2014
+Invoke-Sqlcmd -InputFile ".\RAISERROR.sql" -ServerInstance "192.168.0.250,60016" -username "sa" -password "1qaz@WSX3edc"
+Invoke-Sqlcmd -InputFile ".\RAISERROR.sql" -ServerInstance "192.168.0.250,60017" -username "sa" -password "1qaz@WSX3edc"
